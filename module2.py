8d0d54a10af7f629299461263539738d4d1fc434
prathishdevendran <143081454+prathishdevendran@users.noreply.github.com>
2025-03-28 09:11:33 +0530

    Update module2.py

--- module2.py
+++ module2.py
@@ -12,6 +12,9 @@ class DeadlockVisualizer:
         # Mark nodes and edges involved in a deadlock
         cycle_nodes = set(node for edge in cycles for node in edge)
         cycle_edges = set(cycles)
-      
+
+        node_colors = ['red' if node in cycle_nodes else 'green' for node in graph.nodes()]
+        edge_colors = ['red' if edge in cycle_edges else 'black' for edge in graph.edges()]
+
         nx.draw(graph, pos, with_labels=True, node_color=node_colors, edge_color=edge_colors, node_size=2000, font_size=10)
         plt.show()
